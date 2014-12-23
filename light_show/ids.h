fe5d2bab00ca90a8c0f36da9b8c374a689d7a44c
smellyworkshop <pi@raspberrypi.(none)>
2014-12-23 02:04:10 +0000

    Updated ids.h.

--- light_show/ids.h
+++ light_show/ids.h
@@ -1,3 +1,4 @@
+// updated 2014-08-21 20:18
 // access digital values as bytes or bits (e.g. D1.byte, D1.relay.in2)
 typedef union {
 	struct {
@@ -11,30 +12,51 @@ typedef union {
 		uint8_t in8:1;
 	} relay;
 	uint8_t byte;
-} D_t;
+//        boolean bits[8];
+}  D_t;
+
+//generic message
+typedef struct {
+	uint8_t id;
+        uint8_t res[7];
+} msg_GEN_t;
 
 //packet id defs
 #define ID_M  0x00  //full assignment
 typedef struct {
 	uint8_t id;	// message ID should be set to ID_M
-	D_t D1;		// Arduino #1 relay values for relays 1-8
-	D_t D2;		// Arduino #2 relay values for relays 1-8
-	D_t D3;		// Arduino #3 relay values for relays 1-8
-	uint8_t P1;	// Arduino #4 PWM value for SS switch #1
-	uint8_t P2;	// Arduino #4 PWM value for SS switch #2
-	uint8_t P3;	// Arduino #4 PWM value for SS switch #3
-	uint8_t P4;	// Arduino #4 PWM value for SS switch #4
+        union {
+          struct {
+  	    D_t D1;	// Arduino #1 relay values for relays 1-8
+  	    D_t D2;	// Arduino #2 relay values for relays 1-8
+  	    D_t D3;	// Arduino #3 relay values for relays 1-8
+          };
+          D_t D[3];
+        };
+        union {
+          struct {
+	    uint8_t P1;	// Arduino #4 PWM value for SS switch #1
+	    uint8_t P2;	// Arduino #4 PWM value for SS switch #2
+	    uint8_t P3;	// Arduino #4 PWM value for SS switch #3
+	    uint8_t P4;	// Arduino #4 PWM value for SS switch #4
+          };
+          uint8_t P[4];
+        };
 } msg_M_t;
 
 #define ID_DN 0x10  //digital command to Arduino N
+#define ID_DSN 0x11  //digital command to Arduino N, turn relays indicated by mask on
+#define ID_DCN 0x12  //digital command to Arduino N, turn relays indicated by mask off
 typedef struct {
-	uint8_t id;		// message ID should be set to ID_DN
-	uint8_t N;		// Arduino number
-	D_t D1;			// Arduino #1 relay values for relays 1-8
+	uint8_t id;	// message ID should be set to ID_DN
+	uint8_t N;	// Arduino number
+	D_t D;		// Arduino #1 relay values for relays 1-8
 	uint8_t res[5];	// reserved, set to zero
 } msg_DN_t;
 
 #define ID_D  0x20  //digital command to all binary relay Arduinos
+#define ID_DS  0x21  //digital command to all binary relay Arduinos, turn relays indicated by mask on
+#define ID_DC  0x22  //digital command to all binary relay Arduinos, turn relays indicated by mask off
 typedef struct {
 	uint8_t id;		// message ID should be set to ID_D
 	union {			// allows access via array or name, e.g. D3 or D[3]
@@ -53,28 +75,55 @@ typedef struct {
 
 #define ID_PN 0x30  // send command PWM command to Arduino N
 typedef struct {
-	uint8_t id;		// message ID should be set to ID_PN
-	uint8_t N;		// Arduino number
-	uint8_t P1;		// Arduino #4 PWM value for SS switch #1
-	uint8_t P2;		// Arduino #4 PWM value for SS switch #2
-	uint8_t P3;		// Arduino #4 PWM value for SS switch #3
-	uint8_t P4;		// Arduino #4 PWM value for SS switch #4
+	uint8_t id;	// message ID should be set to ID_PN
+	uint8_t N;	// Arduino number
+        union {
+          struct {
+	    uint8_t P1;	// Arduino N PWM value for SS switch #1
+	    uint8_t P2;	// Arduino N PWM value for SS switch #2
+	    uint8_t P3;	// Arduino N PWM value for SS switch #3
+	    uint8_t P4;	// Arduino N PWM value for SS switch #4
+          };
+          uint8_t P[4];
+        };
 	uint8_t res[2];	// reserved, set to zero
 } msg_PN_t;
 
-#define ID_S  0x40  //status request
+#define ID_PNC 0x40  // send command PWM command to Arduino N channel C
 typedef struct {
-	uint8_t id;		// message ID should be set to ID_PN
-	uint8_t N;		// Arduino number
+	uint8_t id;	// message ID should be set to ID_PN
+	uint8_t N;	// Arduino number
+	uint8_t C;	// Channel number.  0 = all channels
+	uint8_t P;	// Arduino N PWM value for SS switch C
+	uint8_t res[4];	// reserved, set to zero
+} msg_PNC_t;
+
+#define ID_S  0x80  //status request
+typedef struct {
+	uint8_t id;	// message ID should be set to ID_PN
+	uint8_t N;	// Arduino number
 	uint8_t res[6];	// reserved, set to zero
 } msg_S_t;
 
-#define ID_SR  0x50  //status response
+#define ID_SR  0x90  //status response
 typedef struct {
-	uint8_t id;		// message ID should be set to ID_PN
-	uint8_t N;		// Arduino number
+	uint8_t id;	// message ID should be set to ID_PN
+	uint8_t N;	// Arduino number
 	int8_t  temp;   // temperature in Celcius
 	float humidity; // humidity percent
 	uint8_t res[1];	// reserved, set to zero
 } msg_SR_t;
 
+//access a message as any message type
+typedef union {
+        void *raw;
+        uint8_t bytes[8];
+        msg_GEN_t msg_GEN;
+        msg_M_t msg_M;
+        msg_DN_t msg_DN;
+        msg_D_t msg_D;
+        msg_PN_t msg_PN;
+        msg_PNC_t msg_PNC;
+        msg_S_t msg_S;
+        msg_SR_t msg_SR;
+} msg_t;
